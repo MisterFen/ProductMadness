edd5e8f3f002a6c857af564915c1c395c90de0a0
Robbie Chesters <r.chesters@iesohealth.com>
2018-07-26 07:29:34 +0100

    Table Spacing

--- ObjectHandler.py
+++ ObjectHandler.py
@@ -1,9 +1,9 @@
 from Object import Table
 
-table_spacing_x = 200
+table_spacing_x = 165
 table_spacing_y = 200
 
-table1 = Table(50,0)
+table1 = Table(75,0)
 table2 = Table(table1.x+table1.width + table_spacing_x, 0)
 table3 = Table(table2.x+table2.width + table_spacing_x, 0)
 table4 = Table(table1.x, table1.y + table1.height + table_spacing_y)
@@ -11,7 +11,6 @@ table5 = Table(table2.x, table2.y + table1.height + table_spacing_y)
 table6 = Table(table3.x, table3.y + table1.height + table_spacing_y)
 
 objects = [table1, table2, table3, table4, table5, table6]
-
 
 
 def draw_objects(display):
